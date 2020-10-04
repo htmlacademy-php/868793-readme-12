27b8ae8831093d69e8d6db1e35bec96eec9d199a
Гридунова Юлия Сергеевна <ygridunova@tsc.ts>
2020-10-04 19:20:07 +0300

    Добавление постов на страницу

--- index.php
+++ index.php
@@ -2,6 +2,44 @@
 $is_auth = rand(0, 1);
 
 $user_name = 'Юлия'; // укажите здесь ваше имя
+
+$posts = [
+    [
+    'title' => 'Цитата',
+    'type' => 'post-quote',
+    'content' => 'Мы в жизни любим только раз, а после ищем лишь похожих',
+    'name' => 'Лариса',
+    'avatar' => 'userpic-larisa-small.jpg'
+    ],
+    [
+        'title' => 'Игра престолов',
+        'type' => 'post-text',
+        'content' => 'Не могу дождаться начала финального сезона своего любимого сериала!',
+        'name' => 'Владик',
+        'avatar' => 'userpic.jpg'
+    ],
+    [
+        'title' => 'Наконец, обработал фотки!',
+        'type' => 'post-photo',
+        'content' => 'rock-medium.jpg',
+        'name' => 'Виктор',
+        'avatar' => 'userpic-mark.jpg'
+    ],
+    [
+        'title' => 'Моя мечта',
+        'type' => 'post-photo',
+        'content' => 'coast-medium.jpg',
+        'name' => 'Лариса',
+        'avatar' => 'userpic-larisa-small.jpg'
+    ],
+    [
+        'title' => 'Лучшие курсы',
+        'type' => 'post-link',
+        'content' => 'www.htmlacademy.ru',
+        'name' => 'Владик',
+        'avatar' => 'userpic.jpg'
+    ],
+];
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -204,96 +242,97 @@ $user_name = 'Юлия'; // укажите здесь ваше имя
             </div>
         </div>
         <div class="popular__posts">
-            <div class="visually-hidden" id="donor">
-                <!--содержимое для поста-цитаты-->
-                <blockquote>
-                    <p>
-                        <!--здесь текст-->
-                    </p>
-                    <cite>Неизвестный Автор</cite>
-                </blockquote>
-
-                <!--содержимое для поста-ссылки-->
-                <div class="post-link__wrapper">
-                    <a class="post-link__external" href="http://" title="Перейти по ссылке">
-                        <div class="post-link__info-wrapper">
-                            <div class="post-link__icon-wrapper">
-                                <img src="https://www.google.com/s2/favicons?domain=vitadental.ru" alt="Иконка">
-                            </div>
-                            <div class="post-link__info">
-                                <h3><!--здесь заголовок--></h3>
-                            </div>
-                        </div>
-                        <span><!--здесь ссылка--></span>
-                    </a>
+          
+        <?php
+function get_item($input) {
+    $output = '';
+    if ($input['type'] == "post-quote") {
+        return $output = ' <blockquote>
+        <p>' .$input['content'] .'</p>
+        <cite>Неизвестный Автор</cite>
+    </blockquote>';
+    } else if ($input['type'] == "post-text") {
+        return $output = ' <p>'.$input['content'] .'</p>';
+    } else if ($input['type'] == "post-photo") {
+        return $output = ' <div class="post-photo__image-wrapper">
+        <img src="img/'.$input['content'] .'" alt="Фото от пользователя" width="360" height="240">
+    </div>';
+    } else if ($input['type'] == "post-video") {
+        return $output = '  <div class="post-video__block">
+        <div class="post-video__preview">
+            <?=embed_youtube_cover('.$input['content'] .'); ?>
+            <img src="img/coast-medium.jpg" alt="Превью к видео" width="360" height="188">
+        </div>
+        <a href="post-details.html" class="post-video__play-big button">
+            <svg class="post-video__play-big-icon" width="14" height="14">
+                <use xlink:href="#icon-video-play-big"></use>
+            </svg>
+            <span class="visually-hidden">Запустить проигрыватель</span>
+        </a>
+    </div>';
+    } else if ($input['type'] == "post-link") {
+        return $output = ' <div class="post-link__wrapper">
+        <a class="post-link__external" href="http://" title="Перейти по ссылке">
+            <div class="post-link__info-wrapper">
+                <div class="post-link__icon-wrapper">
+                    <img src="https://www.google.com/s2/favicons?domain=vitadental.ru" alt="Иконка">
                 </div>
-
-                <!--содержимое для поста-фото-->
-                <div class="post-photo__image-wrapper">
-                    <img src="img/" alt="Фото от пользователя" width="360" height="240">
+                <div class="post-link__info">
+                    <h3>'.$input['title'] .'</h3>
                 </div>
-
-                <!--содержимое для поста-видео-->
-                <div class="post-video__block">
-                    <div class="post-video__preview">
-                        <?=embed_youtube_cover(/* вставьте ссылку на видео */); ?>
-                        <img src="img/coast-medium.jpg" alt="Превью к видео" width="360" height="188">
-                    </div>
-                    <a href="post-details.html" class="post-video__play-big button">
-                        <svg class="post-video__play-big-icon" width="14" height="14">
-                            <use xlink:href="#icon-video-play-big"></use>
-                        </svg>
-                        <span class="visually-hidden">Запустить проигрыватель</span>
-                    </a>
-                </div>
-
-                <!--содержимое для поста-текста-->
-                <p><!--здесь текст--></p>
             </div>
+            <span>'.$input['content'] .'</span>
+        </a>
+    </div>';
+    } 
+}
+?>
 
-            <article class="popular__post post">
-                <header class="post__header">
-                    <h2><!--здесь заголовок--></h2>
-                </header>
-                <div class="post__main">
-                    <!--здесь содержимое карточки-->
+<?php for ($i = 0; $i < count($posts); $i++) {
+    $post = $posts[$i];
+    echo ' <article class="popular__post post '.$post['type'] .'">
+        <header class="post__header">
+            <h2>'.$post['title'] .'</h2>
+        </header>
+        <div class="post__main">'. get_item($post) .' </div>
+        <footer class="post__footer">
+            <div class="post__author">
+                <a class="post__author-link" href="#" title="Автор">
+                    <div class="post__avatar-wrapper">
+                        <!--укажите путь к файлу аватара-->
+                        <img class="post__author-avatar" src="img/'.$post['avatar'] .'" alt="Аватар пользователя">
+                    </div>
+                    <div class="post__info">
+                        <b class="post__author-name">'.$posts['name'] .'</b>
+                        <time class="post__time" datetime="">дата</time>
+                    </div>
+                </a>
+            </div>
+            <div class="post__indicators">
+                <div class="post__buttons">
+                    <a class="post__indicator post__indicator--likes button" href="#" title="Лайк">
+                        <svg class="post__indicator-icon" width="20" height="17">
+                            <use xlink:href="#icon-heart"></use>
+                        </svg>
+                        <svg class="post__indicator-icon post__indicator-icon--like-active" width="20" height="17">
+                            <use xlink:href="#icon-heart-active"></use>
+                        </svg>
+                        <span>0</span>
+                        <span class="visually-hidden">количество лайков</span>
+                    </a>
+                    <a class="post__indicator post__indicator--comments button" href="#" title="Комментарии">
+                        <svg class="post__indicator-icon" width="19" height="17">
+                            <use xlink:href="#icon-comment"></use>
+                        </svg>
+                        <span>0</span>
+                        <span class="visually-hidden">количество комментариев</span>
+                    </a>
                 </div>
-                <footer class="post__footer">
-                    <div class="post__author">
-                        <a class="post__author-link" href="#" title="Автор">
-                            <div class="post__avatar-wrapper">
-                                <!--укажите путь к файлу аватара-->
-                                <img class="post__author-avatar" src="img/" alt="Аватар пользователя">
-                            </div>
-                            <div class="post__info">
-                                <b class="post__author-name"><!--здесь имя пользоателя--></b>
-                                <time class="post__time" datetime="">дата</time>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="post__indicators">
-                        <div class="post__buttons">
-                            <a class="post__indicator post__indicator--likes button" href="#" title="Лайк">
-                                <svg class="post__indicator-icon" width="20" height="17">
-                                    <use xlink:href="#icon-heart"></use>
-                                </svg>
-                                <svg class="post__indicator-icon post__indicator-icon--like-active" width="20" height="17">
-                                    <use xlink:href="#icon-heart-active"></use>
-                                </svg>
-                                <span>0</span>
-                                <span class="visually-hidden">количество лайков</span>
-                            </a>
-                            <a class="post__indicator post__indicator--comments button" href="#" title="Комментарии">
-                                <svg class="post__indicator-icon" width="19" height="17">
-                                    <use xlink:href="#icon-comment"></use>
-                                </svg>
-                                <span>0</span>
-                                <span class="visually-hidden">количество комментариев</span>
-                            </a>
-                        </div>
-                    </div>
-                </footer>
-            </article>
+            </div>
+        </footer>
+    </article>';
+} ?>
+          
         </div>
     </div>
 </section>
